73a56f41fe577b9caeea146d5985839978092bb2
mebr0 <aza17e@gmail.com>
2019-09-07 21:15:38 +0600

    Change serializers for creating object, create special serializers, add comments

--- order/serializers.py
+++ order/serializers.py
@@ -1,39 +1,43 @@
-from django.shortcuts import get_object_or_404
-
 from rest_framework import serializers
 
 from order.models import Order
 from product.models import Product
-from product.serializers import ProductSerializer
+from product.serializers import ProductOrderSerializer
 from user.serializers import UserSerializer
 
 
+# default serializer for generic view
 class OrderSerializer(serializers.ModelSerializer):
 
+    # status default value is False
     status = serializers.BooleanField(read_only=True)
-    owner = UserSerializer(read_only=True)
-    product = ProductSerializer(read_only=True)
 
-    product_id = serializers.IntegerField(write_only=True)
+    # owner get by OrderManager
+    owner = UserSerializer(read_only=True)
+
+    product = ProductOrderSerializer()
 
     class Meta:
         model = Order
-        fields = ('id', 'count', 'product', 'product_id', 'status', 'owner', )
+        fields = ('id', 'count', 'product', 'status', 'owner', )
 
     def create(self, validated_data):
-        # pop and search product by id
-        product_id = validated_data.pop('product_id')
-        product = get_object_or_404(Product, id=product_id)
 
+        # get product from database
+        product = Product.objects.get(id=validated_data.pop('product')['id'])
+
+        # check whether order's count is greater than product's count
         if product.count < validated_data.get('count'):
             validated_data.pop('count')
-            order = Order.objects.create(product=product, count=-1, **validated_data)
+
+            # set count to -1, because their is not enough products
+            order = Order.objects.create(count=-1, product=product, **validated_data)
 
         else:
+            # decrease product's count by order's count
             product.count -= validated_data.get('count')
             product.save()
 
-            # create product with given product
             order = Order.objects.create(product=product, **validated_data)
 
         return order
--- product/serializers.py
+++ product/serializers.py
@@ -1,32 +1,65 @@
-from django.shortcuts import get_object_or_404
-
 from rest_framework import serializers
 
 from product.models import Category, Product
 
 
+# default serializer for generic view
 class CategorySerializer(serializers.ModelSerializer):
+
+    id = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Category
         fields = ('id', 'name', )
 
 
+# serializer for ProductSerializer and ProductOrderSerializer
+class CategoryProductSerializer(serializers.ModelSerializer):
+
+    id = serializers.IntegerField()
+
+    class Meta:
+        model = Category
+        fields = ('id', 'name', )
+
+
+# default serializer for generic view
 class ProductSerializer(serializers.ModelSerializer):
 
-    category = CategorySerializer(read_only=True)
-    category_id = serializers.IntegerField(write_only=True)
+    id = serializers.IntegerField(read_only=True)
+    category = CategoryProductSerializer()
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'image_url', 'cost', 'count', 'category', 'category_id')
+        fields = ('id', 'name', 'description', 'image_url', 'cost', 'count', 'category', )
 
     def create(self, validated_data):
-        # pop and search category by id
-        category_id = validated_data.pop('category_id')
-        category = get_object_or_404(Category, id=category_id)
 
-        # create product with given category
+        # get category from DB by id
+        category_json = validated_data.pop('category')
+        category = Category.objects.get(id=category_json['id'])
+
+        product = Product.objects.create(category=category, **validated_data)
+
+        return product
+
+
+# serializer for OrderSerializer
+class ProductOrderSerializer(serializers.ModelSerializer):
+
+    id = serializers.IntegerField()
+    category = CategoryProductSerializer()
+
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'image_url', 'cost', 'count', 'category', )
+
+    def create(self, validated_data):
+
+        # get category from DB by id
+        category_json = validated_data.pop('category')
+        category = Category.objects.get(id=category_json['id'])
+
         product = Product.objects.create(category=category, **validated_data)
 
         return product
