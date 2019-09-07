7e1036246cc5ecc7cce17e19ab0cdc06d5536661
mebr0 <aza17e@gmail.com>
2019-09-07 23:03:48 +0600

    Fix __str__ methods for model classes

--- order/models.py
+++ order/models.py
@@ -17,3 +17,6 @@ class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     status = models.BooleanField(default=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} of {} for {}'.format(self.count, self.product, self.owner)
